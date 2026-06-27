e81aeec3d3f71f7ce66acb6459ff19e88e170164
sanjukhatwani99-ux <sanjukhatwani99@gmail.com>
2026-06-27 19:33:51 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+async function getCaseStudies() {
+  const query = `
+    *[_type == "caseStudy"] | order(publishedDate desc){
+      _id,
+      title,
+      shortDescription,
+      projectUrl
+    }
+  `;
+
+  const projectId = "YOUR_PROJECT_ID";
+  const dataset = "production";
+
+  const url = `https://${projectId}.api.sanity.io/v2024-01-01/data/query/${dataset}?query=${encodeURIComponent(query)}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data.result;
+}
+
+function renderCaseStudies(items) {
+  const container = document.getElementById("case-studies");
+
+  container.innerHTML = items
+    .map(
+      (item) => `
+      <div class="card">
+        <h2>${item.title}</h2>
+        <p>${item.shortDescription}</p>
+        <a href="${item.projectUrl}" target="_blank">Open</a>
+      </div>
+    `
+    )
+    .join("");
+}
+
+async function init() {
+  const data = await getCaseStudies();
+
+  console.log("SANITY DATA:", data); // IMPORTANT CHECK
+
+  renderCaseStudies(data);
+}
+
+init();
