425c6bad0ba41c182fd7096c85cd405872639967
sanjukhatwani99-ux <sanjukhatwani99@gmail.com>
2026-06-28 20:35:18 +0530

    Update caseStudy.ts

--- caseStudy.ts
+++ caseStudy.ts
@@ -183,7 +183,7 @@ export const caseStudy = defineType({
         {
           type: 'block',
           styles: [
-            { title: 'Normal',     value: 'normal' },
+            { title: 'H1',     value: 'h1' },
             { title: 'H2',        value: 'h2' },
             { title: 'H3',        value: 'h3' },
           ],
